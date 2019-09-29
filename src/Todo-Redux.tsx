75da6f0ebf39777d218cff4884127dee8af5185e
Ricardo Fretes <rfretes@skytv.co.nz>
2019-09-30 11:12:36 +1300

    moved to hooks

--- src/Todo-Redux.tsx
+++ src/Todo-Redux.tsx
@@ -1,39 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { Box, Button } from "rebass";
-import {  Provider } from 'react-redux';
+import { Provider, useDispatch, useSelector } from "react-redux";
 import { Checkbox, Input, Label } from "@rebass/forms";
 
 import { BorderBox } from "./primitives";
-import { T_Store } from "./types";
-import { connect } from "react-redux";
-import  {store, addTodo, tickTodo } from "./candidates/redux";
+import { T_Store_Redux, T_Todo } from "./types";
+import { store, addTodo, tickTodo } from "./candidates/redux";
 
-const Todo: React.FC<{ store: T_Store; addTodo: any; tickTodo: any }> = ({
-  store,
-  addTodo,
-  tickTodo
-}) => {
+const Todo: React.FC = () => {
   const [input, updateInput] = useState("");
-  const handleAddTodo = () => {
-    addTodo({
-      text: input,
-      done: false,
-      id: Date.now()
-    });
-    updateInput("");
-  };
+  const dispatch = useDispatch();
+  const store = useSelector((state: T_Store_Redux) => state);
+
+  const handleTickTodo = useCallback((payload: T_Todo) => dispatch(tickTodo(payload)), [dispatch]);
+  const handleAddTodo = useCallback(
+    (text: string, done: boolean, id: number) => { dispatch(addTodo({text,done,id})); updateInput("");},
+    [dispatch]
+  );
+
   return (
     <BorderBox>
-      <Input
-        value={input}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => updateInput(e.target.value)}
-      />
-      <Button onClick={handleAddTodo}>Add</Button>
+      <Input value={input} onChange={(e: React.ChangeEvent<HTMLInputElement>) => updateInput(e.target.value)} />
+      <Button onClick={() => handleAddTodo(input, false, Date.now())}>Add</Button>
 
       {store.todos.map(todo => (
         <Box key={todo.id}>
           <Label p={1}>
-            <Checkbox checked={todo.done} onChange={() => tickTodo(todo)} />
+            <Checkbox checked={todo.done} onChange={() => handleTickTodo(todo)} />
             {todo.text}
           </Label>
         </Box>
@@ -42,26 +35,9 @@ const Todo: React.FC<{ store: T_Store; addTodo: any; tickTodo: any }> = ({
   );
 };
 
-// connect
-
-const mapStateToProps = (state: T_Store) => {
-  return {
-    store: state
-  };
-};
-const actionCreators =  {
-  addTodo,
-  tickTodo,
-};
-const ConnectedTodo = connect(
-  mapStateToProps,
-  actionCreators
-)(Todo);
-export default ConnectedTodo;
-
 // Helper to return Redux element
 export const ElementConstructor = () => (
   <Provider store={store}>
-    <ConnectedTodo />
+    <Todo />
   </Provider>
 );
